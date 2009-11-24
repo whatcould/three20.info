3312b20a7ab56775a4153e3b41a6c212738576bc
Jeff Verkoeyen <jverkoey@gmail.com>
2009-11-24 00:40:28 -0800

    Remove default app environment from the site.

--- www/index.php
+++ www/index.php
@@ -6,11 +6,6 @@ defined('APPLICATION_PATH') ||
 defined('LIBRARY_PATH') ||
   define('LIBRARY_PATH', realpath(dirname(__FILE__).'/../library'));
 
-defined('APPLICATION_ENV') ||
-  define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?
-    getenv('APPLICATION_ENV') :
-    'production'));
-
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(LIBRARY_PATH),
     get_include_path(),
